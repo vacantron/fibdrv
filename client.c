08472712c010bff370367fa8f7c4b3d40984b2a3
vacantron <95543602+vacantron@users.noreply.github.com>
2022-03-22 05:41:38 +0800

    Implement 128-bits printf

--- client.c
+++ client.c
@@ -7,11 +7,28 @@
 
 #define FIB_DEV "/dev/fibonacci"
 
+#define UINT64_MAX 0xFFFFFFFFFFFFFFFFULL
+
+void print_u128(int i, __uint128_t u128)
+{
+    unsigned long long lower = u128 & UINT64_MAX;
+    unsigned long long upper = u128 >> 64 & UINT64_MAX;
+
+    if (upper)
+        printf("Reading from " FIB_DEV
+               " at offset %d, returned the sequence "
+               "%llu%llu.\n",
+               i, upper, lower);
+    else
+        printf("Reading from " FIB_DEV
+               " at offset %d, returned the sequence "
+               "%llu.\n",
+               i, lower);
+}
+
 int main()
 {
-    long long sz;
-
-    char buf[1];
+    char *buf;
     char write_buf[] = "testing writing";
     int offset = 100; /* TODO: try test something bigger than the limit */
 
@@ -22,26 +39,21 @@ int main()
     }
 
     for (int i = 0; i <= offset; i++) {
+        long long sz;
         sz = write(fd, write_buf, strlen(write_buf));
         printf("Writing to " FIB_DEV ", returned the sequence %lld\n", sz);
     }
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+        read(fd, buf, sizeof(__uint128_t));
+        print_u128(i, *buf);
     }
 
     for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
-        sz = read(fd, buf, 1);
-        printf("Reading from " FIB_DEV
-               " at offset %d, returned the sequence "
-               "%lld.\n",
-               i, sz);
+        read(fd, buf, sizeof(__uint128_t));
+        print_u128(i, *buf);
     }
 
     close(fd);
